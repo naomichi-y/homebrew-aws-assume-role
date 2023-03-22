5756464e44b8fdbc812f47429a224716aa280f3e
Naomichi Yamakita <n.yamakita@gmail.com>
2023-03-23 01:06:53 +0900

    Update aws-assume-role.rb

--- aws-assume-role.rb
+++ aws-assume-role.rb
@@ -1,6 +1,5 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+require "formula"
+
 class AwsAssumeRole < Formula
   desc ""
   homepage "https://github.com/naomichi-y/aws-assume-role"
