a01a8e0c2e6fac93293421ad2ed4b41d1fb5525c
Naomichi Yamakita <n.yamakita@gmail.com>
2023-03-28 21:15:54 +0900

    Update aws-assume-role.rb

--- aws-assume-role.rb
+++ aws-assume-role.rb
@@ -1,6 +1,7 @@
 require "formula"
 
 class AwsAssumeRole < Formula
+  version "1.0.3"
   homepage "https://github.com/naomichi-y/aws-assume-role"
   url "https://github.com/naomichi-y/aws-assume-role/archive/refs/tags/1.0.3.tar.gz"
   sha256 "c44de58495713ba114a1f8e3f070ce00b3dae39261ef2b58c89d4367fbc9c07b"
